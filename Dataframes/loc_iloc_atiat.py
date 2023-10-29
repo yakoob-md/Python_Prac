bab434d06f9b69720966578ad29d6b31cd561bde
Md Yakub <dabaayakub@gmail.com>
2023-10-29 19:59:13 +0530

    c

--- Dataframes/loc_iloc_atiat.py
+++ Dataframes/loc_iloc_atiat.py
@@ -2,4 +2,20 @@ import pandas as pd
 df=pd.read_csv("Book 1.csv",index_col=0)
 print(df)
 print()
-print(df.loc['2':'4',:])
+# loc is or only laelled 
+# whether the indeices are numbers or not 
+print(df.loc[2:4,:]) 
+print(df.loc[2:4,:])
+print()
+# both these would give the same output
+# random rows then use double box brackets
+print(df.loc[[2,5]])
+print()
+# in iloc positional index is used 
+# whether index is numeric or not 
+# the last provided index will not be retrieved 
+print(df.iloc[1:3,:])
+print()
+# random rows then use double brackets
+print(df.iloc[[1,4]])
+print()

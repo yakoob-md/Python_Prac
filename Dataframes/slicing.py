35f8b8dbb570e0978c812e642f7aa2d02b5add63
Md Yakub <dabaayakub@gmail.com>
2023-10-29 19:37:48 +0530

    slicing df

--- Dataframes/slicing.py
+++ Dataframes/slicing.py
@@ -1,11 +1,17 @@
 import pandas as pd
-import numpy as np
-i=[1,2,3,4,5]
-s=pd.Series(['artha','priya','neil','kein','arjun'],i)
-d=pd.Series(['f','f','m','m','m'],i)
-e=pd.Series([20,40,45,35,48],i)
-dic={'names':s,"sex":d,'marks':e}
-df=pd.DataFrame(dic)
+df=pd.read_csv("Book 1.csv",index_col=0)
 print(df)
-df.to_csv('Book 1.csv')
-print('done')
+print()
+print(df['names'])
+print()
+print(df.names)
+print()
+print(df[['names','sex']])
+print()
+print(df["a":"c"])
+print()
+# 3 index will not be considered 
+print(df[1:3])
+print()
+# only one row 
+print(df[1:2])

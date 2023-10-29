4893e1be91d90301c4da4d30e140cd67b8f356a2
Md Yakub <dabaayakub@gmail.com>
2023-10-29 21:00:03 +0530

    c

--- Graphs/sample.py
+++ Graphs/sample.py
@@ -3,11 +3,13 @@ import matplotlib.pyplot as plt
 x=[2,3,4,5,6]
 y=[100,400,600,50,700]
 z=[300,500,600,530,800]
-plt.plot(x,y,label='hcl')
-plt.plot(x,z,label="hero")
+plt.plot(x,y,label='hcl',color='r',linestyle='dashed',marker='d')
+plt.plot(x,z,label="hero",color='m',linestyle='dotted',marker='*')
 plt.xlabel('data')
 plt.ylabel('points')
 plt.title('data vs points')
 plt.grid()
+plt.xlim(1,7)
+plt.ylim(50,850)
 plt.legend(loc='upper right')
 plt.show()

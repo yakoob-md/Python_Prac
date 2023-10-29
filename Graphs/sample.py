6e08bdce44ec4f6da250d5a0442fe3b59ea2261b
Md Yakub <dabaayakub@gmail.com>
2023-10-29 20:55:20 +0530

    c

--- Graphs/sample.py
+++ Graphs/sample.py
@@ -2,5 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 x=[2,3,4,5,6]
 y=[100,400,600,50,700]
-plt.plot(x,y)
+z=[300,500,600,530,800]
+plt.plot(x,y,label='hcl')
+plt.plot(x,z,label="hero")
+plt.xlabel('data')
+plt.ylabel('points')
+plt.title('data vs points')
+plt.grid()
+plt.legend(loc='upper right')
 plt.show()

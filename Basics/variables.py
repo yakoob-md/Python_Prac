0f371ffe02f127a7afbc359539433d8803adbef9
Md Yakub <dabaayakub@gmail.com>
2023-10-31 19:42:47 +0530

    c

--- Basics/variables.py
+++ Basics/variables.py
@@ -16,4 +16,12 @@ print(sts.mode(d))
 print(np.var(d))
 # SD
 print(math.sqrt(np.var(d)))
-
+# IQR
+print(np.quantile(d,0.75)-np.quantile(d,0.25))
+# Quartiles
+print(np.quantile(d,[0,0.25,0.50,0.75,1]))
+# Range
+print(np.quantile(d,1)-np.quantile(d,0))
+# CV
+std=math.sqrt(np.var(d))
+print(std/mean)

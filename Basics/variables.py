1263147116e41f44309eeb3eb2aa0a1c843e5f64
Md Yakub <dabaayakub@gmail.com>
2023-10-31 23:25:55 +0530

    c

--- Basics/variables.py
+++ Basics/variables.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as sts
+from scipy import stats
 import math 
 d=[200,600,100,50,900,450,500,600,380,250,670]
 # mean
@@ -11,7 +11,7 @@ print(np.sort(d))
 # median
 print(np.median(d))
 # mode
-print(sts.mode(d))
+print(stats.mode(d))
 # variance 
 print(np.var(d))
 # SD

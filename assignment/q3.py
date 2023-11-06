14f2d22cb9ba9de0479577d9c755703cfb67c6a3
Md Yakub <dabaayakub@gmail.com>
2023-11-06 20:05:41 +0530

    c

--- assignment/q3.py
+++ assignment/q3.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df=pd.read_csv('stud.csv.csv')
+# a
 df1=df[df['gender']=='female']
 print(df1.value_counts().sum())
+# b
+g=['group A', 'group B', 'group C', 'group D' ,'group E' ,'group C']
+df2=df[df['race/ethnicity'].isin(g)&(df['gender']=='male')]
+print(df2.value_counts().sum())

11112f2179211716d5824f378192cea40be8cee0
Md Yakub <dabaayakub@gmail.com>
2023-11-06 19:56:01 +0530

    c

--- assignment/q1.py
+++ assignment/q1.py
@@ -4,10 +4,10 @@ df=pd.read_csv("Olympic_medals.csv",encoding='unicode-escape')
 c=['United States','Canada','United Kingdom']
 # a
 df1=df[df["Country"].isin(c)&(df['Medal']=="Gold")]
-print(df1['Gender'].value_counts().sum())
+print(df1['Gender'].value_counts())
 # b 
 df2=df[df['Country'].isin(c)&(df['Medal']=="Gold")&(df['Year']==1976)]
 print(df2["Gender"].value_counts())
 # c
 df3=df[df['Country'].isin(c)&(df['Medal']=="Gold")&(df['Year']==2000)]
-print(df3["Gender"].value_counts().sum())
+print(df3["Gender"].value_counts())
--- assignment/q3.py
+++ assignment/q3.py
@@ -1,6 +1,3 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 df=pd.read_csv('stud.csv.csv')
-s=df['gender']=='female'
-print(s.value_counts())
-groups=[A, B, C, D, 'E' ,'C']

fc9a9a4d3972b04f8f3bdd45cdc49dd6a5bb088f
Md Yakub <dabaayakub@gmail.com>
2023-11-06 19:40:50 +0530

    c

--- assignment/q1.py
+++ assignment/q1.py
@@ -1,4 +1,6 @@
 import pandas as pd
 import numpy as np
-df=pd.read_csv("Olympic_medals.csv")
-print(df.shape)
+df=pd.read_csv("Olympic_medals.csv",encoding='unicode-escape')
+c=['United States','Canada','United Kingdom']
+df1=df[df["Country"].isin(c)&(df['Medal']=="Gold")]
+print(df1['Gender'].value_counts().sum())

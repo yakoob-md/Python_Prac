0b409318a00282732313d09f84175a03e79409d5
Md Yakub <dabaayakub@gmail.com>
2023-11-06 19:16:30 +0530

    c

--- assignment/q3.py
+++ assignment/q3.py
@@ -1,4 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df=pd.read_csv('stud.csv')
-print(df.shape)
+df=pd.read_csv('stud.csv.csv')
+s=df['gender']=='female'
+print(s.value_counts())
+groups=[A, B, C, D, 'E' ,'C']

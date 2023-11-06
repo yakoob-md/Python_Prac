e1fb6a98dfda45ace40ef950c25c0f92d003e943
Md Yakub <dabaayakub@gmail.com>
2023-11-06 20:09:30 +0530

    c

--- assignment/q3.py
+++ assignment/q3.py
@@ -8,3 +8,7 @@ print(df1.value_counts().sum())
 g=['group A', 'group B', 'group C', 'group D' ,'group E' ,'group C']
 df2=df[df['race/ethnicity'].isin(g)&(df['gender']=='male')]
 print(df2.value_counts().sum())
+# c
+df3=df[(df['lunch']=='standard')&(df['gender']=='female')]
+print(df3.value_counts().sum())
+print(df3.shape)

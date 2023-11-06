3f2bf402f19cfdbae9c42e76c38392e47418f8b3
Md Yakub <dabaayakub@gmail.com>
2023-11-06 19:46:00 +0530

    c

--- assignment/q1.py
+++ assignment/q1.py
@@ -2,5 +2,12 @@ import pandas as pd
 import numpy as np
 df=pd.read_csv("Olympic_medals.csv",encoding='unicode-escape')
 c=['United States','Canada','United Kingdom']
+# a
 df1=df[df["Country"].isin(c)&(df['Medal']=="Gold")]
 print(df1['Gender'].value_counts().sum())
+# b 
+df2=df[df['Country'].isin(c)&(df['Medal']=="Gold")&(df['Year']==1976)]
+print(df2["Gender"].value_counts())
+# c
+df3=df[df['Country'].isin(c)&(df['Medal']=="Gold")&(df['Year']==2000)]
+print(df3["Gender"].value_counts().sum())
